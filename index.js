b42201f11e21856f2e57c33b6edaa3117f7cd3ab
motiejus1 <motiejus.baliukonis@vka.lt>
2025-02-04 10:14:53 +0200

    19paskaita

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const express = require('express'); //require pasiima is modules express
 const app = express();
 
 //prisijungima prie duombazes 
+const pool = require('./database');
 
 app.use(express.json());//requestam ir responsem
 
@@ -32,10 +33,65 @@ app.get('/products', async (req, res) => {
     }
     
 });
+//  GET     /users - route mums grazins visus users
+app.get('/users', async (req, res) => {
+    //neapibrezta klaida 400 koda, jeigu nepavyksta prisijungti prie duombazes 500
+    //select * from users
+    
+    try {
+        const results = await pool.query("select * from users");    
+        res.status(200).json(results.rows);
+        // res.status(200).json({ message: 'Sėkmingai pasiekiamas produktų puslapis'});
+    }
+    catch (err) {
+        res.status(400).json({error: 'error'});
+    }
+    
+});
+
+//  GET     /users/:id - route mums viena 1 users
+app.get('/users/:id', async (req, res) => {
+    //neapibrezta klaida 400 koda, jeigu nepavyksta prisijungti prie duombazes 500
+    //select * from users
+    
+    try {
+        const id = req.params.id;
+        const results = await pool.query(`select * from users where id=$1`,[id]);    
+        // const results = await pool.query(`select * from users where id=${id}`);    
+        res.status(200).json(results.rows);
+        // res.status(200).json({ message: 'Sėkmingai pasiekiamas produktų puslapis'});
+    }
+    catch (err) {
+        res.status(400).json({error: 'error'});
+    }
+    
+});
+//  POST         /users - route sukurs users
+app.post('/users', async (req, res) => {
+    try {
+        // insert into users (id,username,"password")  values (1000, 'idetasPerInsert','idetasPerInser')
+        
+        const {id, username, password} = req.body;
+
+        const results = await pool.query(`insert into users (id,username,"password")  values (${id}, '${username}','${password}') returning *`);    
+        // const results = await pool.query(`select * from users where id=${id}`);    
+        res.status(201).json(results.rows[0]);
+        // res.status(200).json({ message: 'Sėkmingai pasiekiamas produktų puslapis'});
+    }
+    catch (err) {
+        res.status(400).json({error: 'error'});
+    }
+    
+});
+//  PUT/PATCH     /users/:id - route redaguos users
+
+//  DELETE     /users/:id  - istrins users
+
+
 
 // /users reikia gauti visų vartotojų sąrašą iš lentelės users
-// 1. API turi prisijungti prie DB
-// 2. Kelia /users
+// 1. API turi prisijungti prie DB x
+// 2. Kelia /users x
 // 3. Per postman atvaizduoti users JSON formatu
 
 
